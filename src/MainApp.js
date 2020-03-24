d6842ad8990e2f8e3cca9bdd8431aae8b95220c8
Diego Plancarte <dplancarte7@gmail.com>
2020-03-23 23:28:46 -0700

    add delete functionality

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,7 +2,7 @@ import React from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import {MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
 
 import Event from "./pages/Event";
 
@@ -22,15 +22,16 @@ class MainApp extends React.Component {
             id: 2,
             time: "10:30",
             title: "Daily Standup Meeting (recurring)",
-            location: "Warsaw Spire Office"
+            location: "Warsaw Spire Office",
         },
         {   
             id: 3, 
             time: "11:00", 
-            title: "Call with HRs" },
+            title: "Call with HRs"
+        },
         {
             id: 4,
-            time: "11:00",
+            time: "12:00",
             title: "Lunch with Jimothy",
             location: "Canteen", 
         }
@@ -38,6 +39,7 @@ class MainApp extends React.Component {
     };
     }
 
+//Without map method
   // myLoopFunction() {
   //   var myArray = [];
   //   var i;
@@ -52,26 +54,53 @@ class MainApp extends React.Component {
   //   return myArray;
   // }
 
+    handleDelete = eventId => {
+        const events = this.state.events.filter(e => e.id !== eventId);
+        this.setState({ events });
+    }
+
     render() {
-    console.log(this.state)
     return (
         <React.Fragment>
         <MDBContainer>
             <MDBRow>
-            <MDBCol md="9">
-                {this.state.events.map(event => (
-                <Event
-                    key={event.id}
-                    id={event.id}
-                    time={event.time}
-                    title={event.title}
-                    location={event.location}
-                    description={event.description}
-                />
-                ))}
+            <MDBCol md="9" className="mb-r">
+                <h2 className="text-uppercase my-3">Today:</h2>
+                <div id="schedule-items">
+                    {this.state.events.map(event => (
+                    <Event
+                        key={event.id}
+                        id={event.id}
+                        time={event.time}
+                        title={event.title}
+                        location={event.location}
+                        description={event.description}
+                        onDelete = {this.handleDelete}
+                    />
+                    ))}
+                </div>
             </MDBCol>
                 <MDBCol md="3">
-            </MDBCol>
+                        <h3 className="text-uppercase my-3">Schedule</h3>
+                        <h6 className="my-3">It's going to be busy today. You have <b>{this.state.events.length} events </b> today.</h6>
+                        <h1 className="my-3">
+                            <MDBRow>
+                                <MDBCol xs="3" className="text-center">
+                                    <MDBIcon icon="sun" fixed />
+                                </MDBCol>
+                                <MDBCol xs="9">Sunny</MDBCol>
+                            </MDBRow>
+                            <MDBRow>
+                                <MDBCol xs="3" className="text-center">
+                                    <MDBIcon icon="thermometer-three-quarters" fixed />
+                                </MDBCol>
+                                <MDBCol xs="9">23°C</MDBCol>
+                            </MDBRow>
+                        </h1>
+                        <p>
+                            Don't forget your sunglasses. Today will be dry sunny, becomming warm in the afternoon with temperatures of between 20 and 25 degrees.
+                        </p>
+                </MDBCol>
             </MDBRow>
         </MDBContainer>
         </React.Fragment>
--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,15 +1,37 @@
 import React from "react";
+import {MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
 
 class Event extends React.Component {
 
     render () {
         const { time, title, location, description } = this.props
-        console.log(this.props)
         return (
             <React.Fragment>
-                <h3>{time} - {title}</h3>
-                <h6>Location: {location}</h6>
-                <p>Desc: {description}</p>
+                <div className="media mt-1">
+                    <h3 className="h3-responsive font-weight-bold mr-3">
+                        {time}
+                    </h3>
+                    <div className="media-body mb-3 md-lg-3">
+                        <MDBBadge 
+                            color="danger" 
+                            className="ml-2 float-right"
+                            onClick={() => this.props.onDelete(this.props.id)}>
+                            -
+                        </MDBBadge>
+                        <h6 className="mt-0 font-weight-bold">{title}</h6>{" "}
+                        <hr className="hr-bold my-2" />
+                        {location && (
+                            <React.Fragment>
+                                <p className="font-smaller mb-0">
+                                    <MDBIcon icon="location-arrow" /> {location}
+                                </p>
+                            </React.Fragment>
+                        )}
+                    </div>
+                </div>
+                {description && (<p className="p-2 mb-4  blue-grey lighten-5 blue-grey lighten-5">Desc: {description}
+                    </p>
+                )}
             </React.Fragment>
         )
     }
